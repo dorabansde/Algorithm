4497e96cf1881e8af85aa0a90e5f7f1a694ab030
DESKTOP-MNR8ONA\fktls <fktlsrja@gmail.com>
2022-03-29 20:13:04 +0900

    uplode

--- Main.java
+++ Main.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
 import java.util.*;
+
 public class Main {	
 	
 		public static void main(String[] args){
 
 			Scanner sc = new Scanner(System.in);
-					
+			int T = sc.nextInt();
+			int H = sc.nextInt();
+			int W = sc.nextInt();
+			int N = sc.nextInt();
+			
 			Stack<Integer> st = new Stack<Integer>();
 
 		
